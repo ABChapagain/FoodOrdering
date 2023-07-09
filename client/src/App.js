d06a1663c1628585ca16e6dd8ccfd1902059434a
saugat21 <saugatbaral122@gmail.com>
2023-07-09 18:54:26 +0545

    react installed and creating layout

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>Food Ordering Website</h1>
+      </Layout>
+
+    </>
+  );
+}
+
+export default App;
