c4ac871d83359e66a8deafc1659363985e3c587e
saugat21 <saugatbaral122@gmail.com>
2023-07-09 17:52:17 +0545

    authentication middleware updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { registerController, loginController } from '../controllers/authController.js';
+import { registerController, loginController, testController } from '../controllers/authController.js';
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 // router object 
 const router = express.Router();
 
@@ -9,6 +10,9 @@ const router = express.Router();
 router.post('/register', registerController);
 
 //FOR LOGIN  || METHOD POST
-router.post('/login', loginController)
+router.post('/login', loginController);
+
+//FOR TEST || METHOD GET
+router.get('/test', requireSignIn, isAdmin, testController);
 
 export default router;
